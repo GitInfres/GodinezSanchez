c103dc0b89de9215fb44cd28d4337eeb4ae24b83
Zayon <Zayon@zayon.eu>
2014-04-02 14:38:52 +0200

    nouvelles spec tetris

--- tetris_rspec.rb
+++ tetris_rspec.rb
@@ -14,4 +14,31 @@ describe "#scope" do
 		[0, 0, 0, 0]]
 		rotation90droite(gridBefore).should eq(gridAfter)
 	end
+	it "Turn left" do
+
+	end
+	it "Generate Random Tetra" do
+
+	end
+	it "Right Translation" do
+
+	end
+	it "Left Translation" do
+
+	end
+	it "Fall Down Tetra" do
+
+	end
+	it "Remove Full Line" do
+
+	end
+	it "Game Over" do
+
+	end
+	it "Collision Detection" do
+
+	end
+	it "Progressive Fall Down" do
+
+	end
 end
